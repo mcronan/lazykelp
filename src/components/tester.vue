bf1880b7d7b5beccd5b9db4baf1329c1aab780de
mcronan <ronanir600@gmail.com>
2023-04-16 21:34:01 +0100

    new

--- src/components/tester.vue
+++ src/components/tester.vue
@@ -127,7 +127,9 @@ export default {
     async fetchForecasts() {
       try {
         //const response = await fetch('https://lazykelp.onrender.com/wave-forecasts');
-        const response = await fetch('http://localhost:3000/wave-forecasts');
+        //const response = await fetch('http://localhost:3000/wave-forecasts');
+        const serverURL = process.env.VUE_APP_SERVER_URL;
+        const response = await fetch(`${serverURL}/wave-forecasts`);
         if (!response.ok) {
           throw new Error('Network response was not ok');
         }
