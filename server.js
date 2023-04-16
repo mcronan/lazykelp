6cf64f5b9f91c0bda7eeddd3fdd1b13e668cdee5
mcronan <ronanir600@gmail.com>
2023-04-16 22:01:18 +0100

    Âjdj

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors());
 // Set up CORS headers
 app.use((req, res, next) => {
    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-    res.setHeader('Access-Control-Allow-Origin', 'https://lazykelp.onrender.com');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
